d2be82b861ba5b8f2785c30815905a71cbaa4e3d
Yasmeen Mohammed <yasmeenshaik0786@gmail.com>
2023-08-09 23:07:46 -0400

    TypeScript Review

---
+++ TypeScript/typescript.js
@@ -0,0 +1,5 @@
+var sum = function (a, b) {
+    return a + b;
+};
+var summation = sum(9, 2);
+console.log(summation);
---
+++ TypeScript/typescript.ts
@@ -0,0 +1,9 @@
+
+const sum = (a: number, b: number) => {
+    return a + b;
+}
+
+
+const summation = sum(9,2)
+
+console.log(summation)
